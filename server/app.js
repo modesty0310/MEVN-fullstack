2e2581b7e14be0e37a8e8c24c51f94a15b77f3b5
modesty0310 <rlatprua4@gmail.com>
2021-08-26 19:26:05 +0900

    문제해결 경로 확인 잘하자

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("uplods"));
+app.use(express.static("uploads"));
 
 // database connect
 mongoose
